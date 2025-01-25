6c54cbf2e595aa5cc4973f3675b1a56729e1339f
Abdalla <aabouelnasr@mail.valenciacollege.edu>
2025-01-25 17:50:36 -0500

    transforming it from dictionary/based info to databases , in proccess of creating db models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from datetime import datetime
 
 # Create your models here.
+class Post(models.Model):
+    author = models.OneToOneField(
+        Author, on_delete=models.CASCADE, null=False
+        )
+    content = models.CharField(max_length=5000)
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=300)
+    date = models.DateField()
+    image = models.ImageField()
+    
+
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=40)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, unique=True)
+
+class Author(models.Model):
+    first_name = models.CharField(
+        max_length=30,)
+    last_name = models.CharField(
+        max_length=30,)
+    age = models.IntegerField(validators=[MinValueValidator(13)])
+    country = models.OneToOneField(Country, on_delete=models.CASCADE, null=True)
+    email = models.EmailField()
+    
+    
